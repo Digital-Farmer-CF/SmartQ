19732e59e3d8cee704af203c64d216f7d691827a
Digital-Farmer-CF <ab188235@foxmail.com>
2025-05-09 22:19:34 +0800

    SmartQ1.8 dec:Front end initialization

--- smartq-fronted/src/components/GlobalHeader.vue
+++ smartq-fronted/src/components/GlobalHeader.vue
@@ -1,7 +1,11 @@
 <template>
   <a-row id="globalHeader" align="center" wrap="false">
     <a-col flex="auto">
-      <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu
+        mode="horizontal"
+        :default-selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
         <a-menu-item
           key="0"
           :style="{ padding: 0, marginRight: '38px' }"
@@ -12,10 +16,9 @@
             <div class="title">SmartQ</div>
           </div>
         </a-menu-item>
-        <a-menu-item key="1">Home</a-menu-item>
-        <a-menu-item key="2">Solution</a-menu-item>
-        <a-menu-item key="3">Cloud Service</a-menu-item>
-        <a-menu-item key="4">Cooperation</a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">{{
+          item.name
+        }}</a-menu-item>
       </a-menu>
     </a-col>
     <a-col flex="100px"
@@ -26,10 +29,27 @@
   </a-row>
 </template>
 
-<script>
-export default {
-  name: "GlobalHeader",
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const selectedKeys = ref(["/"]);
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.push];
+});
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
 };
+const visibleRoutes = routes.filter((item) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
 </script>
 
 <style scoped>
